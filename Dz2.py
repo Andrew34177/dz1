a7b61c4333d82fc425ed389d407ec71b3d22efa2
Andrew34177 <andrey2009khokhlov@gmail.com>
2025-12-02 11:12:21 +0800

    Add files via upload

---
+++ Dz2.py
@@ -0,0 +1,41 @@
+# Задание 1
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+c = int(input("Введите третье число: "))
+summa = a + b + c
+proizvedenie = a * b * c
+print("Сумма чисел:", summa)
+print("Произведение чисел:", proizvedenie)
+
+# Задание 2
+
+r = int(input("Введите заработную плату: "))
+e = int(input("Введите сумму месячного платежа по кредиту: "))
+t = int(input("Введите задолженность по коммунальным услугам: "))
+
+ostatok = r - e - t
+
+print("После всех выплат у вас останется:", ostatok)
+
+# Задание 3
+
+d1 = int(input("Введите длину первой диагонали: "))
+d2 = int(input("Введите длину второй диагонали: "))
+
+a = (d1 * d2) / 2
+
+print("Площадь ромба:", a)
+
+# Задание 4
+
+print("To be")
+print("or not")
+print("to be")
+
+# Задание 4
+
+print("“Life is what happens")
+print("     when")
+print("         you’re busy making other plans”")
+print("                                    John Lennon.")
